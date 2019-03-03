f6dc5be5d0963636da679e751b0bb48d5b8e88a1
Erik Hage <ehage4@gmail.com>
2019-03-02 23:05:31 -0500

    full working version of recipe input tool

--- bin/recipe-input/js/main.js
+++ bin/recipe-input/js/main.js
@@ -6,21 +6,21 @@ $(document).ready(function() {
   function addIngredient() {
     const ingredientsDiv = $('#ingredients');
 
+    ingredientIndex = ingredientIndex + 1;
+
     const newHTML = `
       <div class="fb-text form-group">
-        <p>Ingredient ${(ingredientIndex + 1)}</p>
+        <p>Ingredient ${(ingredientIndex)}</p>
         <label for="ingredientName${ingredientIndex}" class="fb-text-label">Name</label>
         <input type="text" class="form-control" name="ingredientName${ingredientIndex}" id="ingredientName${ingredientIndex}">
         <label for="quantityKind${ingredientIndex}" class="fb-text-label">Quantity Kind</label>
         <input type="text" class="form-control" name="quantityKind${ingredientIndex}" id="quantityKind${ingredientIndex}">
         <label for="quantityValue${ingredientIndex}" class="fb-text-label">Quantity Value</label>
         <input type="number" class="form-control" name="quantityValue${ingredientIndex}" id="quantityValue${ingredientIndex}">
-        <label for="notes${ingredientIndex}" class="fb-text-label">Name</label>
+        <label for="notes${ingredientIndex}" class="fb-text-label">Notes</label>
         <input type="text" class="form-control" name="notes${ingredientIndex}" id="notes${ingredientIndex}">
       </div>
     `;
-
-    ingredientIndex = ingredientIndex + 1;
 
     ingredientsDiv.append($.parseHTML(newHTML));
   };
@@ -28,12 +28,12 @@ $(document).ready(function() {
   function addStep() {
     const stepsDiv = $('#steps');
 
+    stepIndex = stepIndex + 1;
+
     const newHTML = `
-      <label for="stepText${stepIndex}" class="fb-text-label">Step ${(stepIndex + 1)}</label>
+      <label for="stepText${stepIndex}" class="fb-text-label">Step ${(stepIndex)}</label>
       <input type="text" class="form-control" name="stepText${stepIndex}" id="stepText${stepIndex}">
     `;
-
-    stepIndex = stepIndex + 1;
 
     stepsDiv.append($.parseHTML(newHTML));
   };
@@ -62,8 +62,30 @@ $(document).ready(function() {
     const cookTimeValue = Number($('#cookTimeValue').val());
     const yieldKind = $('#yieldKind').val();
     const yieldValue = Number($('#yieldValue').val());
+    const keywords = $('#keywords').val().split(',').filter(keyword => keyword.length > 0);
 
-    const ingredientNodes = $('#ingredients');
+    const ingredients = [];
+    const steps = [];
+
+    for (let i = 1; i <= ingredientIndex; i++) {
+      const ingredient = {
+        quantity: {},
+      };
+      ingredient.name = $(`#ingredientName${i}`).val();
+      ingredient.quantity.kind = $(`#quantityKind${i}`).val();
+      ingredient.quantity.value = Number($(`#quantityValue${i}`).val());
+      ingredient.notes = $(`#notes${i}`).val();
+
+      ingredients.push(ingredient);
+    }
+
+    for (let i = 1; i <= stepIndex; i++) {
+      const step = {};
+      step.id = i;
+      step.text = $(`#stepText${i}`).val();
+
+      steps.push(step);
+    }
 
     const json = {
       recipeId,
@@ -80,14 +102,9 @@ $(document).ready(function() {
         kind: yieldKind,
         value: yieldValue,
       },
-      ingredients: ingredientNodes.map(ingredientNode => ({
-        name: "",
-        quantity: {
-          kind: "",
-          value: 0,
-        },
-        notes: "",
-      })),
+      ingredients,
+      steps,
+      keywords,
     };
 
     $('#outputTextBox').val(JSON.stringify(json, null, 2));
